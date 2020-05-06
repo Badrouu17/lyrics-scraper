1b6ffd8cf575607cc2fa722733df3794d625f519
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-06 06:23:16 +0100

    :bug: heroku h12 fix

--- scrapper.js
+++ scrapper.js
@@ -11,7 +11,13 @@ load_page = function (url) {
 
 function get_browser() {
   return puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--single-process",
+    ],
   });
 }
 
