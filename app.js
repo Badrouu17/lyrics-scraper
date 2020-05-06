48701ab66bbb90db36bb0cba3c379fa417361e94
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-06 04:46:21 +0100

    :tada: Initial commit.

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { getSongLyrics } = require("./scrapper");
+
+// Start express app
+const app = express();
+
+app.enable("trust proxy");
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  })
+);
+
+app.options("*", cors());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use("/api/hello", (req, res) => {
+  res.json({ msg: "hello world!" });
+});
+app.use("/api/v1/genius/getSongLyrics", getSongLyrics);
+
+module.exports = app;
---
+++ scrapper.js
@@ -0,0 +1,93 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+load_page = function (url) {
+  return get_browser()
+    .then(instantiate_page)
+    .then(navigate_page.bind(null, url))
+    .then(fetch_page_content)
+    .catch(restart);
+};
+
+function get_browser() {
+  return puppeteer.launch();
+}
+
+function instantiate_page(browser) {
+  return browser.newPage();
+}
+
+class PageLoadError extends Error {
+  constructor(message, page, address) {
+    super(message);
+    this.page = page;
+    this.address = address;
+  }
+}
+
+function navigate_page(address, page) {
+  return page
+    .goto(address, { waitUntil: "networkidle2", timeout: 240000 })
+    .then(() => Promise.resolve(page))
+    .catch((err) => Promise.reject(new PageLoadError(err, page, address)));
+}
+
+function fetch_page_content(page) {
+  return page.content().then(() => Promise.resolve(page));
+}
+
+function restart(error) {
+  if (error instanceof PageLoadError) {
+    error.page.close();
+    return load_page(error.address);
+  } else {
+    throw error;
+  }
+}
+
+function select_html(page) {
+  return page.$eval(".lyrics", (el) => el.innerHTML);
+}
+
+function print_some_part_of_it(lyrics_html) {
+  let ly;
+  let $ = cheerio.load(lyrics_html, { decodeEntities: false });
+  $("body")
+    .contents()
+    .each((i, e) => {
+      if (e.tagName === "section") {
+        ly = $(e.firstChild);
+      }
+    });
+  ly.contents().each(function (i, elm) {
+    if (elm.tagName === "a") {
+      $(elm).removeAttr("href");
+      $(elm).removeAttr("class");
+      $(elm).removeAttr("data-id");
+      $(elm).removeAttr("ng-click");
+      $(elm).removeAttr("ng-class");
+      $(elm).removeAttr("prevent-default-click");
+      $(elm).removeAttr("annotation-fragment");
+      $(elm).removeAttr("on-hover-with-no-digest");
+      $(elm).removeAttr("classification");
+      $(elm).removeAttr("image");
+      $(elm).removeAttr("pending-editorial-actions-count");
+    } else if (elm.tagName === "defer-compile") {
+      $(elm).remove();
+    }
+  });
+  return ly.html();
+}
+
+exports.getSongLyrics = (req, res, next) => {
+  load_page(req.body.url)
+    .then(select_html)
+    .then(print_some_part_of_it)
+    .then((ly) => {
+      res.status(200).json({
+        status: "success",
+        data: ly,
+      });
+    })
+    .catch((err) => res.status(400).json({ status: "failed", err }));
+};
